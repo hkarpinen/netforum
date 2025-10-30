54b2b0b033f57bf3055549efbdeb918870462039
hkarpinen <karpinenh@gmail.com>
2025-10-29 20:44:04 -0700

    add program entry point, add docker support

---
+++ Program.cs
@@ -0,0 +1,71 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using NETForum.Data;
+using NETForum.Middleware;
+using NETForum.Models;
+using NETForum.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddSassCompiler();
+
+// Add services to the container.
+builder.Services.AddRazorPages();
+
+// Add DB Connection
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseMySql(
+        builder.Configuration.GetConnectionString("DefaultConnection"),
+        ServerVersion.AutoDetect(builder.Configuration.GetConnectionString("DefaultConnection"))
+));
+
+// Add Identity Management
+builder.Services.AddIdentity<User, Role>()
+    .AddEntityFrameworkStores<AppDbContext>()
+    .AddDefaultTokenProviders();
+
+// Configure cookies
+builder.Services.ConfigureApplicationCookie(options =>
+{
+    options.LoginPath = "/Account/Login";
+    options.LogoutPath = "/Account/Logout";
+});
+
+// Add DI classes
+builder.Services.AddScoped<IUserService, UserService>();
+builder.Services.AddScoped<IRoleService, RoleService>();
+builder.Services.AddScoped<IUserRoleService, UserRoleService>();
+builder.Services.AddScoped<IInstallService, InstallService>();
+builder.Services.AddScoped<IForumService, ForumService>();
+builder.Services.AddScoped<IPostService, PostService>();
+builder.Services.AddScoped<IReplyService, ReplyService>();
+builder.Services.AddScoped<ICategoryService, CategoryService>();
+builder.Services.AddScoped<IFileStorageService, LocalFileStorageService>();
+builder.Services.AddScoped<IUserProfileService, UserProfileService>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.UseMiddleware<MigrationMiddleware>();
+app.UseMiddleware<InstallMiddleware>();
+
+app.MapRazorPages();
+
+app.Run();
