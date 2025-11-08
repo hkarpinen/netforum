90c2b3f8aa261a4558d34476860d1f237a26b8a5
hkarpinen <karpinenh@gmail.com>
2025-11-08 13:51:12 -0800

    delete old extensions classes and search criteria

--- Services/Criteria/CategorySearchCriteria.cs
+++
@@ -1,9 +0,0 @@
-namespace NETForum.Services.Criteria;
-
-public class CategorySearchCriteria
-{
-    public string? Name { get; set; }
-    public bool? Published { get; set; } 
-    public string SortBy { get; set; }
-    public bool Ascending { get; set; } = true;
-}
--- Services/Criteria/UserSearchCriteria.cs
+++
@@ -1,9 +0,0 @@
-namespace NETForum.Services.Criteria;
-
-public class UserSearchCriteria
-{
-    public string? Username { get; set; }
-    public string? Email { get; set; }
-    public string SortBy { get; set; }
-    public bool Ascending { get; set; } = true;
-}
